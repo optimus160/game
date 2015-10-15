c2511ab41862c8a3d2a4799fdc61c46bafa01ce8
Vladyslav <vladyslav.p01@gmail.com>
2015-10-15 15:02:51 +0300

    ываыва

--- index.php
+++ index.php
@@ -3,10 +3,14 @@
 namespace abc;
 
 function hello(){
-    return "Bought mother a horse, but horse without leg"
-            ."which wonderful thing, ha-ha";
-}
+    //return "Bought mother a horse, but horse without leg"
+            //."which wonderful thing, ha-ha";
 
+
+    $a = 12;
+
+    return $a;
+}
 echo hello();
 
-//master    
+//master
