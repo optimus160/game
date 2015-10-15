d203dce6906fc80fb3fa424d74caaec67da5143c
Vladyslav <vladyslav.p01@gmail.com>
2015-10-15 15:07:37 +0300

    modified:   index.php

--- index.php
+++ index.php
@@ -3,14 +3,13 @@
 namespace abc;
 
 function hello(){
-    //return "Bought mother a horse, but horse without leg"
-            //."which wonderful thing, ha-ha";
-
-
-    $a = 12;
-
-    return $a;
+    return "Bought mother a horse, but horse without leg"
+            ."which wonderful thing, ha-ha";
 }
-echo hello();
 
+echo hello();
+$i=0;
 //master
+while($i<10){
+    echo $i.PHP_EOL;
+}
